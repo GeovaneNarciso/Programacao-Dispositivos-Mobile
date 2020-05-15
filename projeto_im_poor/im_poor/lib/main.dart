ea07fe2e5a71b9a0bdcca0632e2e20b26632e1be
Carlos Pazuzu <carlosh555@hotmail.com>
2020-05-15 18:10:20 -0300

    Projeto I'm Poor

---
+++ projeto_im_poor/im_poor/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Text("I'm Poor"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Image(
+            image: AssetImage("images/coal.png")
+        ),
+      )
+    )
+  ));
+}
